a29518ebcb93cd83d5feb7c4c718a0cf1d03c9ce
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2024-05-20 08:36:27 -0300

    Add navigation Group

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -17,6 +17,8 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
+    protected static ?string $navigationGroup = 'Admin';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
